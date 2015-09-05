1b9e76825930f3db40d9dabfe3e01e693dea0abc
DennisLiu <dennis.liu@isturay.com>
2015-09-04 17:41:31 -0700

    update

--- src/Visual.py
+++ src/Visual.py
@@ -49,30 +49,43 @@ if __name__ == '__main__':
     # g30 = ggplot(aes(x='x', y='y', color='cluster'), data=result) + geom_point()
     # g30.draw()
     # ax = plt.gca()
+    # plt.ylim((-15, 28))
+    # ax.text(62, 5, 'ARI = 1.0', fontsize=15)
     #
     # for i in range(len(coord_mean)):
     #     ell = Ellipse(xy=(coord_mean[i][0], coord_mean[i][1]), width=12, height=12, color='grey', alpha=0.3)
     #     ax.add_patch(ell)
 
-    # plot ground truth of 100 devices
-    dev_file100 = '../data/dev100.txt'
+    # # plot ground truth of 100 devices
+    # dev_file100 = '../data/dev100v2.txt'
+    # cls_sep100 = [20, 50, 75, 100]
+    # dev_data, dev_label = load_data(dev_file100, head_name, cls_sep100)
+    # coord, pc = pca_coord(dev_data, head_name[0:31])
+    # result = pd.DataFrame({'x': coord[:, 0], 'y': coord[:, 1], 'cluster': dev_label})
+    #
+    # g100 = ggplot(aes(x='x', y='y', color='cluster'), data=result) + geom_point()
+    # g100.draw()
+
+    # plot ground truth of v2 100 devices
+    dev_file100v2 = '../data/dev100v2.txt'
     cls_sep100 = [20, 50, 75, 100]
-    dev_data, dev_label = load_data(dev_file100, head_name, cls_sep100)
+    dev_data, dev_label = load_data(dev_file100v2, head_name, cls_sep100)
     coord, pc = pca_coord(dev_data, head_name[0:31])
     result = pd.DataFrame({'x': coord[:, 0], 'y': coord[:, 1], 'cluster': dev_label})
 
-    g100 = ggplot(aes(x='x', y='y', color='cluster'), data=result) + geom_point()
-    g100.draw()
+    dev100v2_mean = pd.read_csv('../data/dev100v2clsmean.txt', names=head_name[0:31])
+    coord_mean = pc.transform(dev100v2_mean)
 
-    # # plot ground truth of v2 100 devices
-    # dev_file100v2 = '../data/dev100v2.txt'
-    # cls_sep100 = [20, 50, 75, 100]
-    # dev_data, dev_label = load_data(dev_file100v2, head_name, cls_sep100)
-    # coord = pca_coord(dev_data, head_name[0:31])
-    # result = pd.DataFrame({'x': coord[:, 0], 'y': coord[:, 1], 'cluster': dev_label})
-    #
-    # g100v2 = ggplot(aes(x='x', y='y', color='cluster'), data=result) + geom_point()
-    # g100v2.draw()
+    g100v2 = ggplot(aes(x='x', y='y', color='cluster'), data=result) + geom_point()
+    g100v2.draw()
+    ax = plt.gca()
+
+    for i in range(len(coord_mean)):
+        ell = Ellipse(xy=(coord_mean[i][0], coord_mean[i][1]), width=12, height=12, color='grey', alpha=0.3)
+        ax.add_patch(ell)
+    plt.xlim((-30, 30))
+    plt.ylim((-8, 8))
+    ax.text(32, 0, 'ARI = 0.67', fontsize=15)
 
     plt.show()
 
